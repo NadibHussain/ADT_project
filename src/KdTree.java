ae2ba8ec0d1f14274bbf0079a43973d43f255d65
Tanzia Ahmed <52648080+tanzia-ahmed@users.noreply.github.com>
2022-03-13 12:56:52 -0400

    print function added

--- src/KdTree.java
+++ src/KdTree.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class KdTree {
     private int dimensions;
-    private Node root = null;
+    public Node root = null;
 
     public KdTree(List<Node> nodes) {
         create_tree(nodes);
@@ -43,5 +43,61 @@ public class KdTree {
             }
         }
     }
+
+    public void printTree(Node root) {
+        LinkedList<Node> tree = new LinkedList<>();
+        tree.add(root);
+        LinkedList<Node> temp = new LinkedList<>();
+        int height = getHeightOfTree(root)-1;
+        //System.out.println(height);
+        double numberOfElements = (Math.pow(2 ,(height + 1)) - 1);
+        //System.out.println(numberOfElements);
+        int counter = 0;
+        while (counter <= height) {
+            Node node = tree.removeFirst();
+            if (temp.isEmpty()) {
+                printSpace(numberOfElements / Math.pow(2 , counter + 1), node);
+            } else {
+                printSpace(numberOfElements / Math.pow(2 , counter), node);
+            }
+            if (node == null) {
+                temp.add(null);
+                temp.add(null);
+            } else {
+                temp.add(node.left);
+                temp.add(node.right);
+            }
+
+            if (tree.isEmpty()) {
+                System.out.println("");
+                System.out.println("");
+                tree = temp;
+                temp = new LinkedList<>();
+                counter++;
+            }
+
+        }
+    }
+
+    public static void printSpace(double n, Node node){
+        for(;n>0;n--) {
+            System.out.print("\t");
+        }
+        if(node == null){
+            System.out.print(" ");
+        }
+        else {
+            System.out.print(node);
+        }
+    }
+
+    public static int getHeightOfTree(Node root){
+        if(root==null){
+            return 0;
+        }
+        return 1+ Math.max(getHeightOfTree(root.left), getHeightOfTree(root.right));
+    }
+
+
 
 }
--- src/index.java
+++ src/index.java
@@ -8,7 +8,13 @@ public class index {
         list.add(new Node(new int[]{121, 1422}));
         list.add(new Node(new int[]{22, 1342}));
         list.add(new Node(new int[]{132, 1322}));
+        list.add(new Node(new int[]{1440, 1422}));
+        list.add(new Node(new int[]{1350, 1500}));
+        list.add(new Node(new int[]{1320, 1322}));
+        list.add(new Node(new int[]{1350, 1100}));
+        list.add(new Node(new int[]{1320, 1422}));
 
         KdTree kd = new KdTree(list);
+        kd.printTree(kd.root);
     }
 }
